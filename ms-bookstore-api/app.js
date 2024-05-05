97eb34046b6644a41186b110cb3bd6f4205026fd
Piampoon Phoonpiam <piampoon.phoonpiam@tech.jago.com>
2024-05-05 23:58:45 +0700

    add example book store api

--- ms-bookstore-api/app.js
+++ ms-bookstore-api/app.js
@@ -1,8 +1,50 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const app = express();
+app.use(bodyParser.json());
+
+let books = [
+    { id: 1, title: "1984", author: "George Orwell" },
+    { id: 2, title: "The Great Gatsby", author: "F. Scott Fitzgerald" }
+];
+
+app.get('/books', (req, res) => {
+    res.status(200).json(books);
+});
+
+app.get('/books/:id', (req, res) => {
+    const book = books.find(b => b.id === parseInt(req.params.id));
+    if (!book) return res.status(404).send('The book with the given ID was not found.');
+    res.status(200).json(book);
+});
+
+app.post('/books', (req, res) => {
+    const book = {
+        id: books.length + 1,
+        title: req.body.title,
+        author: req.body.author
+    };
+    books.push(book);
+    res.status(201).send(book);
+});
+
+app.put('/books/:id', (req, res) => {
+    const book = books.find(b => b.id === parseInt(req.params.id));
+    if (!book) return res.status(404).send('The book with the given ID was not found.');
+
+    book.title = req.body.title;
+    book.author = req.body.author;
+    res.status(200).send(book);
+});
+
+app.delete('/books/:id', (req, res) => {
+    const book = books.find(b => b.id === parseInt(req.params.id));
+    if (!book) return res.status(404).send('The book with the given ID was not found.');
+
+    const index = books.indexOf(book);
+    books.splice(index, 1);
+    res.status(204).send();
 });
 
 const PORT = process.env.PORT || 3000;
